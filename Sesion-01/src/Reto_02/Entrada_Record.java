34e0f1b03aa61deab8c767809466151452d75ec6
Mariana <marianaalmodovarg19m@gmail.com>
2025-05-15 02:55:20 -0600

    Delete Sesion-01 directory

--- Sesion-01/src/Reto_02/Entrada_Record.java
+++
@@ -1,7 +0,0 @@
-package Reto_02;
-
-public record Entrada_Record(String evento, double precio) {
-    public void mostrarInformacion() {
-        System.out.println("Evento: " + evento + " | Precio: $" + precio);
-    }
-}
